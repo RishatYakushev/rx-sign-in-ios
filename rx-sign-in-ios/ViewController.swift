58d88ca93cfcdb313b3896053fb1e71311becacc
Rishat Yakushev <rishat.yakushev@flatstack.com>
2019-04-14 20:48:00 +0300

    Initialized

---
+++ rx-sign-in-ios/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  rx-sign-in-ios
+//
+//  Created by Rishat Yakushev on 14/04/2019.
+//  Copyright © 2019 Rishat Yakushev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
